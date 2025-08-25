77fffe8f304021f8ed90de3f4cb7fd12557555e5
harshborude <harshborude11@gmail.com>
2025-08-25 09:50:13 +0530

    added mongoDB connection and api responses

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import express from "express";
 import dotenv from 'dotenv';
 import app from './app.js';
-
+import connectDB from './db/index.js';
 
 dotenv.config({
   path: "./.env",
@@ -18,7 +18,15 @@ const PORT = process.env.PORT || 3000;
 // })
 
 
-
+connectDB()
+  .then(()=>{
+    
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
+
+  })
+  .catch((err)=>{
+    console.error("MongoDB connection error", err)
+    process.exit(1)
+  })
